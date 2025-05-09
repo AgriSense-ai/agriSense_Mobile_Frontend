a37223a6e1fb7f5f13990372390ffee964d23502
ConradKash <kakurucon1234@gmail.com>
2025-05-09 17:31:52 +0300

    Worked on the background of image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,95 +1,52 @@
 import { Image } from "expo-image";
-import {
-  Platform,
-  StyleSheet,
-  View,
-  Text,
-  ImageBackground,
-} from "react-native";
+import { StyleSheet, View, ImageBackground } from "react-native";
 
-import { HelloWave } from "@/components/HelloWave";
-import { ThemedText } from "@/components/ThemedText";
-import ProgressPie from "@/components/indexPage/progressView/Progress";
 import CardLayout from "@/components/indexPage/progressCards/cardLayout";
 import ProgressLayout from "@/components/indexPage/progressView/progressLayout";
 import ServicesLayout from "@/components/indexPage/servicesCenter/servicesLayout";
 import NavBarLayout from "@/components/navBar/navBarLayout";
+import { Colors } from "../../constants/Colors";
 
 const backgroundImage = require("@/assets/images/Background.png");
 
 export default function HomeScreen() {
   return (
-    // <ParallaxScrollView
-    //   headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-    //   headerImage={
-    //     <Image
-    //       source={require('@/assets/images/partial-react-logo.png')}
-    //       style={styles.reactLogo}
-    //     />
-    //   }>
-    //   <ThemedView style={styles.titleContainer}>
-    //     <ThemedText type="title">Welcome!</ThemedText>
-    //     <HelloWave />
-    //   </ThemedView>
-    //   <ThemedView style={styles.stepContainer}>
-    //     <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-    //     <ThemedText>
-    //       Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-    //       Press{' '}
-    //       <ThemedText type="defaultSemiBold">
-    //         {Platform.select({
-    //           ios: 'cmd + d',
-    //           android: 'cmd + m',
-    //           web: 'F12',
-    //         })}
-    //       </ThemedText>{' '}
-    //       to open developer tools.
-    //     </ThemedText>
-    //   </ThemedView>
-    //   <ThemedView style={styles.stepContainer}>
-    //     <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-    //     <ThemedText>
-    //       {`Tap the Explore tab to learn more about what's included in this starter app.`}
-    //     </ThemedText>
-    //   </ThemedView>
-    //   <ThemedView style={styles.stepContainer}>
-    //     <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-    //     <ThemedText>
-    //       {`When you're ready, run `}
-    //       <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-    //       <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-    //       <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-    //       <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-    //     </ThemedText>
-    //   </ThemedView>
-    // </ParallaxScrollView>
-    <View
+    <ImageBackground
+      source={backgroundImage}
+      resizeMode="cover"
       style={{
         flex: 1,
-        justifyContent: "flex-end",
+        justifyContent: "center",
         alignItems: "center",
-        paddingTop: 50,
+        backgroundColor: Colors.light.background,
+      }}
+      imageStyle={{
+        opacity: 0.2,
       }}
     >
-      <ImageBackground
-        source={backgroundImage}
-        resizeMode="cover"
-        style={styles.reactLogo}
-      ></ImageBackground>
-      <NavBarLayout />
       <View
         style={{
           flex: 1,
-          justifyContent: "flex-start",
+          justifyContent: "flex-end",
           alignItems: "center",
           paddingTop: 50,
         }}
       >
-        <ProgressLayout />
-        <CardLayout />
-        <ServicesLayout />
+        <NavBarLayout />
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "flex-start",
+            alignItems: "center",
+            paddingTop: 50,
+          }}
+        >
+          <ProgressLayout />
+          <CardLayout />
+          <ServicesLayout />
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
@@ -104,7 +61,6 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    flex: 1,
     width: "100%",
     height: "100%",
     position: "absolute",
