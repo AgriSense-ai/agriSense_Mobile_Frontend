a3ba49cc7871c52525e37e36ca2d06b01b127e73
ConradKash <kakurucon1234@gmail.com>
2025-05-30 23:55:40 +0300

    Refactor: Add authentication screens to TabLayout component

--- app/(authentication)/_layout.tsx
+++ app/(authentication)/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function TabLayout() {
-  return <Stack screenOptions={{ headerShown: false }}></Stack>;
+  return <Stack screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="index" />
+    <Stack.Screen name="sign-in" />
+    <Stack.Screen name="sign-up" />
+    <Stack.Screen name="forgot-password" />
+    <Stack.Screen name="reset-password" />ß
+  </Stack>;
 }
